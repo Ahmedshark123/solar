b7057a264c9a348c3d398766e9430dc3062bdba9
ahmedemad <aeahmedaeae@gmail.com>
2024-08-05 19:14:54 +0300

    this is the committed

---
+++ week1/Task2/main.js
@@ -0,0 +1,42 @@
+const slides = document.querySelectorAll(".slide");
+
+let counter = 0;
+
+
+slides.forEach(
+    (slide, index) => {
+    return    slide.style.left = `${index * 100}%`
+
+    }
+)
+
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+            slide.style.transistion = '0.3s all ease'
+        }
+    )
+}
+
+
+
+
+const goPrev = () => {
+    counter--;
+    slideImage();
+}
+
+const goNext = () => {
+    counter++;
+    slideImage();
+}
+
+
+
+
+
+
+
+
